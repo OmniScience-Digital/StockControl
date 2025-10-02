38a423cf92bf68c757c37494d1c840fb31e4aec1
ThabisoBlessed <thabiso@omniscience.digital>
2025-10-02 11:33:10 +0200

    landing database

---
+++ app/landing/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useEffect } from "react";
+import Navbar from "@/components/layout/navbar";
+import Footer from "@/components/layout/footer";
+import Home from "./Components/dynamicLanding";
+
+
+
+export default function Landing() {
+    return (
+        <div className="flex flex-col h-screen bg-background text-foreground">
+            <Navbar/>
+
+      <main className="flex-1 p-1 mt-20 ">
+        <Home />
+      </main>
+        </div>
+    )
+}
