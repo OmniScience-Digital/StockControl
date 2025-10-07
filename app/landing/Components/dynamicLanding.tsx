0757b5d8708f7f878c5182b0de35e221e5ce211c
ThabisoBlessed <thabiso@omniscience.digital>
2025-10-07 09:21:09 +0200

    Landing delete

--- app/landing/Components/dynamicLanding.tsx
+++ app/landing/Components/dynamicLanding.tsx
@@ -22,12 +22,16 @@ export default function Home() {
     const [open, setOpen] = useState(false);
     const [opendelete, setOpendelete] = useState(false);
     const [name, setName] = useState("");
-    const [loading, setLoading] = useState(true);
+    const [loading, setLoading] = useState(false);
     const [dataArray, setData] = useState<Dashboard[]>([]);
     const [searchQuery, setSearchQuery] = useState("");
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [dashboardToDelete, setDashboardToDelete] = useState<number | null>(null);
 
+    // Debug before useEffect
+
+console.log("Client models:", client?.models);
+console.log("Landing model:", client?.models?.Landing);
 
     // Fetch dashboards
     useEffect(() => {
