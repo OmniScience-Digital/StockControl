3fec0989f34d58644af5ba105f42372620a387e7
ThabisoBlessed <thabiso@omniscience.digital>
2025-10-21 17:33:09 +0200

    Deleting invalie route field

--- app/api/vifclick-up/route.ts
+++ app/api/vifclick-up/route.ts
@@ -1,8 +1,6 @@
 import * as constants from "@/app/constants";
 import { NextResponse } from "next/server";
 
-export const maxBodySize = '50mb';
-
 export async function POST(req: Request) {
   try {
     // Extract the raw body (form-data)
