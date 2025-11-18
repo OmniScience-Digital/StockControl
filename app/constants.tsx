b711c36afa172e49b0d13232ab0b25063bcf7945
OmniScience-Digital <techstack@omniscience.digital>
2025-11-18 09:59:44 +0200

    Handling test url

--- app/constants.tsx
+++ app/constants.tsx
@@ -1,6 +1,7 @@
 export const baseUrl = "http://localhost:5001/api/v1";
 // export const securebaseUrlprod = "https://unb298qh1g.execute-api.us-east-2.amazonaws.com/api/v1";
-export const securebaseUrltest ="https://apqirzaiib.execute-api.us-east-1.amazonaws.com/api/v1";
+export const securebaseUrltest ="https://wq3qo9l3de.execute-api.us-east-1.amazonaws.com/api/v1";
+export const securebaseUrlProd ="https://apqirzaiib.execute-api.us-east-1.amazonaws.com/api/v1";
 export const API_TOKEN = 'pk_230674953_NCM5RWFNCTW278728K0DP79NHZNII0HN';
 export const LIST_ID = '901213458480';
 export const USERNAME_FIELD_ID = 'daf6f996-8096-473b-b9e4-9e20f4568d63';
